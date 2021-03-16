cfe4620b89d171e5954e1beb7ec83e1294436ee6
Rezyapkin <rezyapkin@yandex.ru>
2021-03-16 08:58:58 +0300

    complete task1

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <html>
-<title>Домашнее задание к уроку 4</title>
+<title>Домашнее задание к уроку 7</title>
 <body>
-<h1>Домашнее задание к уроку 4</h1>
-<ul>
-<?php for($lesson=1; file_exists('task'.$lesson); $lesson++): ?>
-<li><a href="'task'.$lesson">Задание <?=$lesson?></a></li>
-<?php endfor; ?>
-</ul>
+<h1>Домашнее задание к уроку 7</h1>
+<h3>1. Найти и указать в проекте Front Controller и расписать классы, которые с ним взаимодействуют.</h3>
+<p>Front Controller - Kernel (app/Kernel.php)</p>
+<p>Класс ControllerResolver нужен для поиска нужного контроллера. Сами контроллеры находятся в пространстве именн controller (MainController, OrderController, ProductController, UserController).</p>
+<p>Класс ArgumentResolver получает нужный метод (action), который нужно вызвать в классе контроллера.</p>
+<p>В качестве параметров фронт контроллер принимает экземпляр класса Request, а возвращает Response (сгенерированная страница).</p>
+<p>P.S. Надеюсь описал то, что нужно</p>
 </body>
 </html>
