56ad2e1e8b316830e3d32bc6f8c20c9f3b173cd2
Rezyapkin <rezyapkin@yandex.ru>
2021-03-16 22:42:59 +0300

    complete task1

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <html>
-<title>Домашнее задание к уроку 4</title>
+<title>Домашнее задание к уроку 9</title>
 <body>
-<h1>Домашнее задание к уроку 4</h1>
+<h1>Домашнее задание к уроку 9</h1>
 <ul>
 <?php for($lesson=1; file_exists('task'.$lesson); $lesson++): ?>
-<li><a href="'task'.$lesson">Задание <?=$lesson?></a></li>
+<li><a href="task<?=$lesson?>">Задание <?=$lesson?></a></li>
 <?php endfor; ?>
 </ul>
 </body>
